e4808cafb12bc12562baa50b3c28ca42fcb44cc5
SB- Alumno <SB-Alumno@duoc.acad>
2026-04-28 10:16:39 -0400

    feat: 5 microservicios base para AventuraRide completados

---
+++ services/cliente/src/test/java/cl/duoc/aventuraride/cliente/ClienteApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.duoc.aventuraride.cliente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClienteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ services/reserva/src/main/java/cl/duoc/aventuraride/reserva/ReservaApplication.java
@@ -0,0 +1,13 @@
+package cl.duoc.aventuraride.reserva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservaApplication.class, args);
+	}
+
+}
