7e3ced1bbf0f0b42499d9e274ef88a205d2f7221
NikZett <n.ibanezm@duocuc.cl>
2026-05-17 23:31:47 -0400

    Se agregan microservicios faltantes: facturacion, notificacion, soporte, promocion y resena

---
+++ services/promocion/src/test/java/cl/duoc/aventuraride/promocion/PromocionApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.duoc.aventuraride.promocion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PromocionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ services/soporte/src/test/java/cl/duoc/aventuraride/soporte/SoporteApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.duoc.aventuraride.soporte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoporteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
